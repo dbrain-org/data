84329d810198131bf2cacc33b6b0e4f2efc849d8
Eric Poitras <eric.poitras@gmail.com>
2015-11-08 21:35:24 -0500

    Added jaxrs integration

---
+++ src/main/java/org/dbrain/data/jaxrs/ValueBodyReader.java
@@ -0,0 +1,48 @@
+package org.dbrain.data.jaxrs;
+
+import org.dbrain.data.TextSerializer;
+import org.dbrain.data.Value;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+
+/**
+ * Simple body writer that stream the element to the entity,
+ */
+@javax.ws.rs.ext.Provider
+public class ValueBodyReader implements MessageBodyReader<Value> {
+
+    final TextSerializer serializer;
+
+    public ValueBodyReader( TextSerializer serializer ) {
+        this.serializer = serializer;
+    }
+
+    /**
+     * Return the charset from the mediatype. If none, default to UTF-8.
+     */
+    private static final Charset getCharset(MediaType m) {
+        String name = (m == null) ? null : m.getParameters().get("charset");
+        return (name == null) ? Charset.forName("UTF-8") : Charset.forName(name);
+    }
+
+    @Override
+    public boolean isReadable( Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType ) {
+        return Value.class.isAssignableFrom( type );
+    }
+
+    @Override
+    public Value readFrom( Class<Value> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream ) throws IOException, WebApplicationException {
+        InputStreamReader isr = new InputStreamReader( entityStream, getCharset( mediaType ) );
+        return serializer.read( isr, Value.class );
+    }
+
+}
