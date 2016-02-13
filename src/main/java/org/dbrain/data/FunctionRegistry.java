b7c1d5cf89e77f38fda99a5a4ff3aceb3293d6a0
epoitras <eric.poitras@gmail.com>
2016-02-12 23:08:54 -0500

    Type Resolvers

---
+++ src/main/java/org/dbrain/data/FunctionRegistry.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright [2016] [Eric Poitras]
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package org.dbrain.data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * A function registry. Will return the function that process the specific message class.
+ */
+public class FunctionRegistry<T> {
+
+    /**
+     * Start building a registry.
+     */
+    public static final <T> Builder<T> newBuilder() {
+        return new Builder();
+    }
+
+
+    private final Map<Class<? extends T>, Function> functionByClass;
+
+
+    private FunctionRegistry( Map<Class<? extends T>, Function> nameMap ) {
+        this.functionByClass = nameMap;
+    }
+
+    /**
+     * @return The function handling the specific class (or superclass)
+     */
+    @SuppressWarnings( "unchecked" )
+    public <U extends T, R> Function<U, R> get( Class<U> clazz ) {
+        Objects.requireNonNull( clazz );
+        return functionByClass.get( clazz );
+    }
+
+
+    /**
+     * Utility class to build new TypeRegistry instance.
+     *
+     * Note: This type registry builder should not be reused to build multiple TypeRegistry instance. Use individual instance for each required registry.
+     */
+    public static class Builder<T> {
+
+        private final Map<Class<? extends T>, Function> functionByClass = new HashMap<>();
+
+        /**
+         * Build a new type registry with only subclasses of the specific Base Class.
+         */
+        Builder() {
+        }
+
+        /**
+         * Register a new function that takes the specific class.
+         */
+        public <U extends T, R> Builder<T> add( Class<? extends T> clazz, Function<U, R> fun ) {
+            Objects.requireNonNull( clazz );
+            functionByClass.put( clazz, fun );
+            return this;
+        }
+
+        /**
+         * @return A new function registry.
+         */
+        public FunctionRegistry<T> build() {
+            return new FunctionRegistry( functionByClass );
+        }
+
+    }
+
+}
